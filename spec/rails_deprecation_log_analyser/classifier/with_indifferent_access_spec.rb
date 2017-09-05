59b290a31b34c7feb5ea5c10c05e090369f30f38
randomcodenz <randomcodenz@users.noreply.github.com>
2017-09-05 20:42:27 +1200

    Extract shared classifier behaviours
    Extract the shared classifier behaviours from with_indifferent_access
    classifier spec and move them into a shared example in the same file in
    preparation for creating a shared example that can be used for all
    classifiers.

--- spec/rails_deprecation_log_analyser/classifier/with_indifferent_access_spec.rb
+++ spec/rails_deprecation_log_analyser/classifier/with_indifferent_access_spec.rb
@@ -4,13 +4,16 @@ require 'spec_helper'
 
 module RailsDeprecationLogAnalyser
   module Classifier
-    RSpec.describe WithIndifferentAccess do
+    RSpec.shared_examples 'a deprecation warning classifier' do
       let(:deprecation_leader) { 'DEPRECATION WARNING:' }
       let(:method) { 'assign_nested_attributes_for_one_to_one_association' }
       let(:file) { '/var/lib/jenkins/gems/ruby/2.3.0/gems/protected_attributes_continued-1.3.0/lib/active_record/mass_assignment_security/nested_attributes.rb' }
       let(:line_number) { '53' }
-      let(:cleaned_log_line) { "Method with_indifferent_access is deprecated and will be removed in Rails 5.1, as `ActionController::Parameters` no longer inherits from hash. Using this deprecated behavior exposes potential security problems. If you continue to use this method you may be creating a security vulnerability in your app that can be exploited. Instead, consider using one of these documented methods which are not deprecated: http://api.rubyonrails.org/v5.0.3/classes/ActionController/Parameters.html (called from #{method} at #{file}:#{line_number})" }
+      let(:cleaned_log_line) { "#{deprecation_warning} (called from #{method} at #{file}:#{line_number})" }
       let(:deprecation_log_line) { "#{deprecation_leader} #{cleaned_log_line}" }
+      let(:deprecated) { 'Method with_indifferent_access on ActionController::Parameters' }
+      let(:summary) { 'Method with_indifferent_access on ActionController::Parameters is deprecated. Using this deprecated behavior exposes potential security problems.' }
+      let(:message) { 'Method with_indifferent_access on ActionController::Parameters is deprecated. Instead, consider using one of these documented methods which are not deprecated: http://api.rubyonrails.org/v5.0.3/classes/ActionController/Parameters.html' }
       let(:lines) { [deprecation_log_line, 'Random log line 1', 'Random log line 2'] }
       let(:filter) { double('filter') }
       let(:classifier_result) { classifier.process(lines, filter) }
@@ -18,13 +21,13 @@ module RailsDeprecationLogAnalyser
       subject(:classifier) { described_class.new('') }
 
       describe '#match?' do
-        context 'when the log line is a with_indifferent_access deprecation warning' do
+        context 'when the log line contains the deprecation warning' do
           it 'matches' do
             expect(classifier.match?(deprecation_log_line)).to be true
           end
         end
 
-        context 'when the log line is not a with_indifferent_access deprecation warning' do
+        context 'when the log line does not match the deprecation warning' do
           let(:cleaned_log_line) { "random deprecation message. This behavior is deprecated and will be changed with Rails 5.1 to only do something else. Use #other_thing instead. (called from #{method} at #{file}:#{line_number})" }
 
           it 'does not match' do
@@ -36,20 +39,29 @@ module RailsDeprecationLogAnalyser
       describe '#process' do
         before { allow(filter).to receive(:clean).with(deprecation_log_line).and_return(cleaned_log_line) }
 
-        it 'returns a with_indifferent_access deprecation warning' do
+        it 'returns a deprecation warning' do
           expect(classifier_result.deprecation_warning).to have_attributes(
-            :deprecated => 'Method with_indifferent_access on ActionController::Parameters',
-            :summary => 'Method with_indifferent_access on ActionController::Parameters is deprecated. Using this deprecated behavior exposes potential security problems.',
-            :message => 'Method with_indifferent_access on ActionController::Parameters is deprecated. Instead, consider using one of these documented methods which are not deprecated: http://api.rubyonrails.org/v5.0.3/classes/ActionController/Parameters.html',
+            :deprecated => deprecated,
+            :summary => summary,
+            :message => message,
             :method => method,
             :file => file,
             :line_number => line_number
           )
         end
 
-        it 'returns the with_indifferent_access deprecation warning log line' do
+        it 'returns the deprecation warning log line' do
           expect(classifier_result.log_lines).to eq [deprecation_log_line]
         end
+      end
+    end
+
+    RSpec.describe WithIndifferentAccess do
+      it_behaves_like 'a deprecation warning classifier' do
+        let(:deprecation_warning) { 'Method with_indifferent_access is deprecated and will be removed in Rails 5.1, as `ActionController::Parameters` no longer inherits from hash. Using this deprecated behavior exposes potential security problems. If you continue to use this method you may be creating a security vulnerability in your app that can be exploited. Instead, consider using one of these documented methods which are not deprecated: http://api.rubyonrails.org/v5.0.3/classes/ActionController/Parameters.html' }
+        let(:deprecated) { 'Method with_indifferent_access on ActionController::Parameters' }
+        let(:summary) { 'Method with_indifferent_access on ActionController::Parameters is deprecated. Using this deprecated behavior exposes potential security problems.' }
+        let(:message) { 'Method with_indifferent_access on ActionController::Parameters is deprecated. Instead, consider using one of these documented methods which are not deprecated: http://api.rubyonrails.org/v5.0.3/classes/ActionController/Parameters.html' }
       end
     end
   end

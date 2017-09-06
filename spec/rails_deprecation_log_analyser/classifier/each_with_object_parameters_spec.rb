149a51ebf9f89509d7cd293aac7d83d9b37ca70f
Michael Winton <michael.winton@powershop.co.nz>
2017-09-06 20:38:55 +1200

    Implement 'EachWithObjectParameters' classifier

---
+++ spec/rails_deprecation_log_analyser/classifier/each_with_object_parameters_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'rails_deprecation_log_analyser/classifier/shared_examples'
+
+module RailsDeprecationLogAnalyser
+  module Classifier
+    RSpec.describe EachWithObjectParameters do
+      it_behaves_like 'a deprecation warning classifier' do
+        let(:deprecation_warning) { 'Method each_with_object is deprecated and will be removed in Rails 5.1, as `ActionController::Parameters` no longer inherits from hash. Using this deprecated behavior exposes potential security problems. If you continue to use this method you may be creating a security vulnerability in your app that can be exploited. Instead, consider using one of these documented methods which are not deprecated: http://api.rubyonrails.org/v5.0.3/classes/ActionController/Parameters.html' }
+        let(:deprecated) { 'ActionController::Parameters#each_with_object' }
+        let(:summary) { 'Method each_with_object is deprecated and will be removed in Rails 5.1, as `ActionController::Parameters` no longer inherits from hash. Using this deprecated behavior exposes potential security problems. If you continue to use this method you may be creating a security vulnerability in your app that can be exploited.' }
+        let(:message) { 'Method each_with_object is deprecated and will be removed in Rails 5.1, as `ActionController::Parameters` no longer inherits from hash. Using this deprecated behavior exposes potential security problems. If you continue to use this method you may be creating a security vulnerability in your app that can be exploited. Instead, consider using one of these documented methods which are not deprecated: http://api.rubyonrails.org/v5.0.3/classes/ActionController/Parameters.html' }
+      end
+    end
+  end
+end
